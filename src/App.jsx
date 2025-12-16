da959690e2413ef4bdfb4dd8d78680cac11ea705
Michele Sifanno <michelesifanno.ms@gmail.com>
2025-12-16 23:52:59 +0100

    Sist def

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { HelmetProvider } from "react-helmet-async";
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import { router } from './routes/Routing';
-import { GoogleAnalytics } from './config/GoogleAnalytics';
 
 
 function App() {
   return (
     <HelmetProvider>
       <RouterProvider router={router} />
-      <GoogleAnalytics />
     </HelmetProvider>
   )
 }
